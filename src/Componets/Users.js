eac9015d2bac48b360123ce5e1cc6e3e42f837b1
RoseveIT <62Bip23_Sorta557_qq>
2022-04-09 00:07:39 +0300

    hw1

--- src/Componets/Users.js
+++ src/Componets/Users.js
@@ -1,15 +1,15 @@
 import {useState, useEffect} from 'react';
-
+import User from "./User"
 const Users = () => {
     const {users, setUsers} = useState([]);
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(value => value.json())
-            .then(data => setUsers(data))
+            .then(value => setUsers(value))
     },[])
     return (
         <div>
-            {users.map(data=><div>{data.name}</div>)}
+            {users.map(user=><User key={user.id} user={user} />)}
         </div>
     );
 };
