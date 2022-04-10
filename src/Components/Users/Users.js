305ec798ef86c8d5f57d556dbaf4f04a7aae73bf
RoseveIT <62Bip23_Sorta557_qq>
2022-04-11 00:28:29 +0300

    hw2

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -11,7 +11,7 @@ const Users = () => {
 
     return (
         <div>
-            {users.map(user=><User key={user.id} id={user.id} name={user.name} username={user.username} />)}
+            {users.map(user=><User key={user.id} user={user} />)}
         </div>
     );
 };
