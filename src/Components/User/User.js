387d3ee99bee56c6e347ed4941640cce34630c29
RoseveIT <62Bip23_Sorta557_qq>
2022-04-11 12:58:46 +0300

    hw2

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const User = ({user}) => {
-
+    const {id, user, username} = user;
     return (
         <div>
-            {user.id} - {user.name} - {user.username}
+            <span>{id} - {name} - {username}</span>
         </div>
     );
 };
