1b3eb78ba80008c4197768ea9ce7eb2444845dd7
RoseveIT <62Bip23_Sorta557_qq>
2022-04-21 21:38:43 +0300

    hw5_part2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import './App.css';
 
-function App() {
+import {useReducer} from "react";
+
+const init=(initCount)=>{
+  return {
+    count1:initCount,
+    count2:initCount
+  }
+}
+
+const  reducer = (state, action)=>{
+    switch (action.type){
+      case 'inc':
+        return{...state, count1:state.count1+1}
+      case 'dec':
+        return{...state, count1:state.count1-1}
+      case 'reset':
+        return{...state, count1:0}
+      case 'set':
+        return {...state, count1:action.payload}
+
+    }
+}
+
+const App =()=> {
+  const [state, dispatch] = useReducer(reducer, 0, init);
+
+  // const inc = () =>{
+  //     dispatch({type:'inc'})
+  // }
   return (
     <div>
-
+      {state.count1}
+      <button onClick={()=>dispatch({type:'inc'})}>inc</button>
+      <button onClick={()=>dispatch({type:'dec'})}>dec</button>
+      <button onClick={()=>dispatch({type:'reset'})}>reset</button>
+      <button onClick={()=>dispatch({type:'set', payload:10})}>set to 10</button>
     </div>
   );
 }
