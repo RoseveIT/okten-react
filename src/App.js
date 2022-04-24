7197d6e8cd8e2787f68134266ae52e798ef8b8ab
RoseveIT <62Bip23_Sorta557_qq>
2022-04-24 16:01:48 +0300

    hw6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import {useDispatch, useSelector} from "react-redux";
+
 function App() {
+    let state = useSelector(state => state);
+
+    let dispatch = useDispatch()
   return (
     <div>
-
+        <h2>State is - {state}</h2>
+        <button onClick={()=>{
+            dispatch({type:'inc', payload: 1})
+        }}>Inc</button>
+        <button onClick={()=>{
+            dispatch({type:'dec', payload: 1})
+        }}>Dec</button>
+        <button onClick={()=>{
+            dispatch({type:'reset', payload: 0})
+        }}>Reset</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
+function reducer(state=0, action){
+    // eslint-disable-next-line default-case
+    switch (action.type){
+        case 'inc':
+            return state + action.payload;
+        case 'dec':
+            return state - action.payload;
+        case 'reset':
+            state = action.payload
+            return state;
+        default:
+            return state
+    }
+}
+const store = createStore(reducer);
+
+
+root.render(<Provider store={store}><App/></Provider>)
